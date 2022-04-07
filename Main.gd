246465e8071d4e39140edcfb6f5e06e2a83160a0
V-Gira <virgile.girardin@gmail.com>
2022-04-07 20:32:06 +0200

    create simple layout

---
+++ Main.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Theme_button_up():
+	pass # Replace with function body.
